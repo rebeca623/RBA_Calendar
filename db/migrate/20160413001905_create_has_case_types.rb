f3f11b8d11f6b99b8d9e3bd7c56e86f45451f830
Saul Green <saulgreen7@hotmail.com>
2016-08-09 13:48:48 -0700

    Changed relationship from employees to users

--- db/migrate/20160413001905_create_has_case_types.rb
+++ db/migrate/20160413001905_create_has_case_types.rb
@@ -1,7 +1,7 @@
 class CreateHasCaseTypes < ActiveRecord::Migration
   def change
     create_table :has_case_types do |t|
-      t.references :employee, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.references :case_type, index: true, foreign_key: true
 
       t.timestamps null: false
