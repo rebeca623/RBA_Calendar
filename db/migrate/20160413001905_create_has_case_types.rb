b88bded338e0003307c384500eca536863a9a419
Saul Green <saulgreen7@hotmail.com>
2016-08-09 17:38:10 -0700

    Modified employee relationship

--- db/migrate/20160413001905_create_has_case_types.rb
+++ db/migrate/20160413001905_create_has_case_types.rb
@@ -4,7 +4,7 @@ class CreateHasCaseTypes < ActiveRecord::Migration
       t.references :user, index: true, foreign_key: true
       t.references :case_type, index: true, foreign_key: true
 
-      t.timestamps null: false
+      t.timestamps null: false 
     end
   end
-end
+end 
